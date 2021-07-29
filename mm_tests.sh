aac7b7fdba36fae65fe5cda6ea1a36594ae056b3
RobinAxelsson <axelsson.robin@outlook.com>
2021-07-29 16:49:42 +0200

    error works as well!

--- mm_tests.sh
+++ mm_tests.sh
@@ -2,8 +2,12 @@ JS_CMD="node mm_bash.js"
 
 testLang(){
     local T1="1 2 3 4 5 10 14" #last two is expected output
+    #local T1="0 2 3 4 5 10 14" #error
     local T2="10 11 12 13 14 46 50"
-    local ARR=("$T1" "$T2")
+    local T3="7 69 2 221 8974 299 9271"
+    local T4="1 1 1 1 1 4 4"
+    local T5="1000000000 1000000000 1000000000 1000000000 1000000000 4000000000 4000000000"
+    local ARR=("$T1" "$T2" "$T3" "$T4")
     local SCORE=0
 
     for T in "${ARR[@]}"; do { 
@@ -11,7 +15,6 @@ testLang(){
         ((SCORE+=$?));
     } 
     done
-
     echo $1 $SCORE"/${#ARR[@]}"
 }
 testCase(){
@@ -19,41 +22,10 @@ testCase(){
     if [[ $OUT == ${@:8:2} ]]; then
         return 1
     else
+        echo FAILED: ${@}
+        echo EXPECTED: ${@:8:2} ACTUAL: $OUT
         return 0
     fi
 }
-# testCase(){
-#     local OUT=$("${@:1:7}")
-#     if [[ $OUT == ${@:8:2} ]]; then
-#         echo Passed
-#     else
-#         echo Fail
-#     fi
-# }
 
 testLang "$JS_CMD"
-
-#function with input command, output
-
-# function testCase(input, expectedMin, expectedMax){
-#     let actual = miniMaxSum(input)
-#     let minActual = actual[0]
-#     let maxActual = actual[1]
-#     if (expectedMin == minActual && expectedMax == maxActual) return true
-#     else{
-#         console.log(`Expected:${expectedMin},${expectedMax} Actual:${actual}`)
-#         return false
-#     }
-# }
-
-# testResults = [
-#     testCase([1,2,3,4,5], 10, 14),
-#     testCase([10,11,12,13,14], 46, 50),
-#     testCase([7,69,2,221,8974], 299, 9271),
-#     testCase([1000000000, 1000000000, 1000000000, 1000000000, 1000000000], 4000000000, 4000000000),
-#     testCase([1,1,1,1,1], 4, 4)
-# ]
-
-# let passed = testResults.filter(t => t == true).length
-
-# console.log(`Passed: ${passed}/${testResults.length}`)
