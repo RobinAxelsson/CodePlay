3739da5c72fc4104e62309ca6afe51ed66890028
RobinAxelsson <axelsson.robin@outlook.com>
2021-08-04 18:59:42 +0200

    function in bash done

--- tree.sh
+++ tree.sh
@@ -1,23 +1,63 @@
 #!/bin/bash
 # art of _ underscores and 1 ones
 # a tree with maximum
-
-WIDTH=15
-CENTER=$((WIDTH / 2))
-echo $CENTER
-HEIGHT=5
-STRING=""
-for ((y = 0; y < HEIGHT; y++)); do
-    for ((x = 0; x < WIDTH; x++)); do
-        if [[ $x == $CENTER ]]; then
-            STRING="${STRING}1"
-        else
-            STRING="${STRING}_"
+printLine() {
+    local length=$1
+    shift
+    local args=("$@")
+    local string=""
+    for ((i = 0; i < length; i++)); do
+        local oneTrue=false
+        for T in "${args[@]}"; do
+            {
+                if [[ $T == "$i" ]]; then
+                    string="${string}1"
+                    oneTrue=true
+                fi
+            }
+        done
+        if [[ $oneTrue = false ]]; then
+            string="${string}_"
         fi
     done
+    echo $string
+}
+WIDTH=15
+#CENTER=$((WIDTH / 2))
+ARRAY=(2 3)
+#HEIGHT=5
+HEIGHT=1
+STRING=""
+for ((y = 0; y < HEIGHT; y++)); do
+    #STRING=$(printLine $WIDTH $CENTER)
+    STRING=$(printLine $WIDTH "${ARRAY[@]}")
     ROWS[$y]="$STRING"
     STRING=""
 done
 for i in "${ROWS[@]}"; do
-    printf "%s\n" $i
+    printf "%s\n" "$i"
 done
+
+# VAR=$(printLine 5 1 2)
+# echo $VAR
+
+### STRAIGHT LINE PRINT! ###
+# WIDTH=15
+# CENTER=$((WIDTH / 2))
+# echo $CENTER
+# HEIGHT=5
+# STRING=""
+# for ((y = 0; y < HEIGHT; y++)); do
+#     for ((x = 0; x < WIDTH; x++)); do
+#         if [[ $x == $CENTER ]]; then
+#             STRING="${STRING}1"
+#         else
+#             STRING="${STRING}_"
+#         fi
+#     done
+#     ROWS[$y]="$STRING"
+#     STRING=""
+# done
+# for i in "${ROWS[@]}"; do
+#     printf "%s\n" $i
+# done
