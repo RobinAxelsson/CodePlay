cbf44e997b81f3ddeacc2c520272f58944d11118
RobinAxelsson <axelsson.robin@outlook.com>
2021-08-25 22:44:53 +0200

    about to start new parse with dictionary

--- romannumerals.ts
+++ romannumerals.ts
@@ -1,5 +1,5 @@
 //var persons: { [id: string] : IPerson; } = {};
-const rDict = Object.freeze({
+const dict = Object.freeze({
   I: 1,
   V: 5,
   X: 10,
@@ -7,49 +7,104 @@ const rDict = Object.freeze({
   C: 100,
   D: 500,
   M: 1000,
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  20: "XX",
+  30: "XXX",
+  40: "XL",
+  50: "L",
+  60: "LX",
+  70: "LXX",
+  80: "LXXX",
+  90: "XC",
+  100: "C",
+  200: "CC",
+  300: "CCC",
+  400: "CD",
+  500: "D",
+  600: "DC",
+  700: "DCC",
+  800: "DCCC",
+  900: "CM",
+  1000: "M",
 });
+
 let sum = (numerals, multiplyer) =>
-  [...numerals].reduce((prev, n) => prev + rDict[n] * multiplyer, 0);
+  [...numerals].reduce((prev, n) => prev + dict[n] * multiplyer, 0);
 
 let add = (numerals) => sum(numerals, 1);
 let sub = (numerals) => sum(numerals, -1);
-
 let minusChars = (numerals) =>
-  [...numerals].reduce((prev, n, i, arr) =>
-    i === arr.length - 1
-      ? prev
-      : rDict[n] < rDict[arr[i + 1]]
-      ? (prev += n)
-      : prev
+  [...numerals].reduce(
+    (prev, n, i, arr) =>
+      i === arr.length - 1
+        ? prev
+        : dict[n] < dict[arr[i + 1]]
+        ? (prev += n)
+        : prev,
+    ""
   );
-let min = minusChars("XLIV");
-min;
 let totalSum = (numerals) => sub(minusChars(numerals)) * 2 + add(numerals);
-
-let div = (a, b) => Math.floor(a / b);
-
-let test = "X".repeat(3);
-test;
-
-let parse = (d, r) => ({
-  roman: r.repeat(Math.floor(d / rDict[r])),
-  rest: d % rDict[r],
-});
-//let concat = (d, arr) => ({roman: r.repeat(Math.floor(d/rDict[r])), rest: d % rDict[r]})
 function recursion(acc: string, rlist: string[], rest: number) {
-  console.log(acc);
-  console.log(rlist);
-  console.log(rest);
   if (rest === 0) return acc; //Base case
-  acc += rlist[0].repeat(Math.floor(rest / rDict[rlist[0]]));
-  rest = rest % rDict[rlist[0]];
+  acc += rlist[0].repeat(Math.floor(rest / dict[rlist[0]]));
+  rest = rest % dict[rlist[0]];
   if (rlist.length === 1) return acc;
   return recursion(acc, rlist.slice(1), rest);
 }
-let list = ["M", "D", "C", "L", "X", "V", "I"];
-let romanRec = (x) => recursion("", list, x);
-let test2 = romanRec(1111);
-test2;
+// const recursion = (acc: string, rlist: string[], rest: number) =>
+//   rest === 0 || rlist.length === 0
+//     ? acc
+//     : recursion(
+//         (acc += rlist[0].repeat(Math.floor(rest / rDict[rlist[0]]))),
+//         rlist.slice(1),
+//         rest % rDict[rlist[0]]
+//       );
 
-//let translate = (x) =>
-//["M","D","C","L","X","V","I"].reduce((rn, char) => (), {roman:"", rest:x});
+//#region romanRec
+let romanRec = (x) => recursion("", ["M", "D", "C", "L", "X", "V", "I"], x);
+let test2 = romanRec(4);
+let test3 = romanRec(9);
+let test4 = romanRec(14);
+let test5 = romanRec(99);
+test2;
+test3;
+test4;
+test5;
+//#endregion
+
+let getNext = (x: string) =>
+  ["I", "V", "X", "L", "C", "D", "M"].reduce(
+    (acc, n, i, arr) => (x === n ? [...acc, arr[i + 1]] : acc),
+    []
+  )[0];
+let romeSub = (m, n) => dict[m] - dict[n];
+let romeAdd = (m, n) => dict[m] + dict[n];
+
+test4 = getNext("I");
+test4;
+function tryParse(s: string, comp: string) {
+  let nextVal = dict[comp];
+  let sum = add(s);
+  let countPrefix = nextVal - sum;
+  let prefix = s[0].repeat(countPrefix / dict[s[0]]);
+  if (prefix.length > 1) return s;
+  else return prefix + comp;
+}
+test4 = tryParse("IIIIIIIII", "X");
+test4;
+//LXXXXVIIII 99 XCIX
+
+//IV 5-1
+//VI
+//IIII
+//VIIII
+//
