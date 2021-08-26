696cd668079bcb7df8e394099075f195a76fdf2e
RobinAxelsson <axelsson.robin@outlook.com>
2021-08-26 14:09:21 +0200

    ending commit

--- romannumerals.js
+++ romannumerals.js
@@ -1,4 +1,4 @@
-const dict = Object.freeze({
+const romeToDeci = {
   I: 1,
   V: 5,
   X: 10,
@@ -6,6 +6,8 @@ const dict = Object.freeze({
   C: 100,
   D: 500,
   M: 1000,
+};
+const deciToRome = {
   1: "I",
   2: "II",
   3: "III",
@@ -34,89 +36,83 @@ const dict = Object.freeze({
   800: "DCCC",
   900: "CM",
   1000: "M",
-});
-
-let sum = (numerals, multiplyer) =>
-  [...numerals].reduce((prev, n) => prev + dict[n] * multiplyer, 0);
-
-let add = (numerals) => sum(numerals, 1);
-let sub = (numerals) => sum(numerals, -1);
-let minusChars = (numerals) =>
-  [...numerals].reduce(
-    (prev, n, i, arr) =>
-      i === arr.length - 1
-        ? prev
-        : dict[n] < dict[arr[i + 1]]
-        ? (prev += n)
-        : prev,
-    ""
-  );
-let totalSum = (numerals) => sub(minusChars(numerals)) * 2 + add(numerals);
-let multiplyer = (x) => 10 ** x.toString().length;
-let floored = (x) =>
-  Math.floor(x / 10 ** x.toString().length) * 10 ** x.toString().length;
-function parse(acc, numb) {
-  if (numb === 0) return acc;
-  if (numb.toString().length > 3) {
-    let r = Math.floor(numb / 1000);
-    acc += "M".repeat(r);
-    numb -= 1000 * acc.length;
-    return parse(acc, numb);
+};
+function deciParse(x) {
+  let numerals = [...x];
+  let dec = 0;
+  while (numerals.length > 1) {
+    if (romeToDeci[numerals[0]] < romeToDeci[numerals[1]]) {
+      dec -= romeToDeci[numerals[0]];
+    } else {
+      dec += romeToDeci[numerals[0]];
+    }
+    numerals = numerals.slice(1);
   }
-  let base = 10 ** (numb.toString().length - 1);
-  let current = Math.floor(numb / base) * base;
-  return parse((acc += dict[current]), (numb -= current));
+  return dec + romeToDeci[numerals[0]];
 }
-function formatRoman(numerals) {
-  let deci = totalSum(numerals);
-  return parse("", deci);
+function romanParse(x) {
+  let numeral = "";
+  while (x > 0) {
+    if (x.toString().length > 3) {
+      let r = Math.floor(x / 1000);
+      numeral += "M".repeat(r);
+      x -= 1000 * numeral.length;
+    } else {
+      let base = 10 ** (x.toString().length - 1);
+      let current = Math.floor(x / base) * base;
+      numeral += deciToRome[current];
+      x -= current;
+    }
+  }
+  return numeral;
+}
+const formatRoman = (x) => romanParse(deciParse(x));
+
+//TESTS
+const expectEqual = function (actual, expected) {
+  if (actual === expected) return 1;
+  console.log(`false - expected: ${expected} actual: ${actual}`);
+  return 0;
+};
+function testAsserts(asserts) {
+  console.log(
+    `Passed: ${asserts.reduce((prev, r) => (prev += r), 0)}/${asserts.length}`
+  );
 }
 function testSuite() {
   console.log("----------------");
-  const expectEqual = function (actual, expected) {
-    if (actual === expected) {
-      return 1;
-    }
-    console.log(`false - expected: ${expected} actual: ${actual}`);
-    return 0;
-  };
-  function getTotal(asserts) {
-    console.log(
-      `Passed: ${asserts.reduce((prev, r) => (prev += r), 0)}/${asserts.length}`
-    );
-  }
-  getTotal([
-    expectEqual(totalSum("MMMMMMMMMMMMMIIII"), 13004),
-    expectEqual(totalSum("CCXX"), 220),
-    expectEqual(totalSum("LLLXXXXX"), 200),
-    expectEqual(totalSum("XCIX"), 99),
-    expectEqual(totalSum("LXXIX"), 79),
-    expectEqual(totalSum("L"), 50),
-    expectEqual(totalSum("L"), 50),
-    expectEqual(totalSum("VVVVVVVVV"), 45),
-    expectEqual(totalSum("IIIII"), 5),
+  testAsserts([
+    expectEqual(deciParse("MMMMMMMMMMMMMIIII"), 13004),
+    expectEqual(deciParse("CCXX"), 220),
+    expectEqual(deciParse("LLLXXXXX"), 200),
+    expectEqual(deciParse("XCIX"), 99),
+    expectEqual(deciParse("LXXIX"), 79),
+    expectEqual(deciParse("L"), 50),
+    expectEqual(deciParse("L"), 50),
+    expectEqual(deciParse("VVVVVVVVV"), 45),
+    expectEqual(deciParse("IIIII"), 5),
     expectEqual(formatRoman("MMMMMMMMMMMMMIIII"), "MMMMMMMMMMMMMIV"),
     expectEqual(formatRoman("CCXX"), "CCXX"),
     expectEqual(formatRoman("LLLXXXXX"), "CC"),
     expectEqual(formatRoman("VVVVVVVVV"), "XLV"),
     expectEqual(formatRoman("IIIII"), "V"),
     expectEqual(
-      parse("", 1000000),
+      romanParse(1000000),
       "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
     ),
-    expectEqual(parse("", 10000), "MMMMMMMMMM"),
-    expectEqual(parse("", 9999), "MMMMMMMMMCMXCIX"),
-    expectEqual(parse("", 2000), "MM"),
-    expectEqual(parse("", 1000), "M"),
-    expectEqual(parse("", 500), "D"),
-    expectEqual(parse("", 100), "C"),
-    expectEqual(parse("", 99), "XCIX"),
-    expectEqual(parse("", 79), "LXXIX"),
-    expectEqual(parse("", 50), "L"),
-    expectEqual(parse("", 10), "X"),
-    expectEqual(parse("", 1), "I"),
-    expectEqual(parse("", 4), "IV"),
-    expectEqual(parse("", 9), "IX"),
+    expectEqual(romanParse(10000), "MMMMMMMMMM"),
+    expectEqual(romanParse(9999), "MMMMMMMMMCMXCIX"),
+    expectEqual(romanParse(2000), "MM"),
+    expectEqual(romanParse(1000), "M"),
+    expectEqual(romanParse(500), "D"),
+    expectEqual(romanParse(100), "C"),
+    expectEqual(romanParse(99), "XCIX"),
+    expectEqual(romanParse(79), "LXXIX"),
+    expectEqual(romanParse(50), "L"),
+    expectEqual(romanParse(10), "X"),
+    expectEqual(romanParse(1), "I"),
+    expectEqual(romanParse(4), "IV"),
+    expectEqual(romanParse(9), "IX"),
   ]);
 }
 testSuite();
--- watch.sh
+++ watch.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-#fswatch -0 ~/Code/CodeChallanges/RomanNumerals/src/ | node ./src/romanNumerals.js
-#fswatch -0 ~/Code/CodeChallanges/RomanNumerals/src/
-#fswatch ~/Code/CodeChallanges/RomanNumerals/src/
-# fswatch -or ~/Code/CodeChallanges/RomanNumerals/src/ | xargs -n1 -I{} echo hello
-# fswatch -or ~/Code/CodeChallanges/RomanNumerals/src/romannumerals.js | xargs -n1 -I{} echo hello
-# fswatch -or ~/Code/CodeChallanges/RomanNumerals/src/romannumerals.js | xargs -n1 -I{} node ~/Code/CodeChallanges/RomanNumerals/src/romannumerals.js
 fswatch -or ./romannumerals.js | xargs -n1 -I{} node ./romannumerals.js
