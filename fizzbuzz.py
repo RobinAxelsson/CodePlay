6439e00a35f2b9b5f7a87e66704f76cc0eaf4523
RobinAxelsson <axelsson.robin@outlook.com>
2021-09-01 13:07:20 +0200

    intial

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for i in range(1,101):
+ a = ''
+ if not i%3:
+  a+= 'Fizz'
+ if i%5:
+   a+= 'Buzz'
+ if a == '':
+   a = str(i)
+ print(a)
+
+#for i in range(1,101):print("Fizz"*(i%3<1)+(i%5<1)*"Buzz"or i)
