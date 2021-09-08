d53799a5e4a150f7988a0fc7f9e9f0b3ca23fc61
RobinAxelsson <axelsson.robin@outlook.com>
2021-09-08 16:39:24 +0200

    added missbehave script-doc

---
+++ script_docs/git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+### git push force change in remote
+# git push --force origin {branch}
+
+### git reset hard before rebase
+# git reset --hard ORIG_HEAD
+
+### join commit with last commit (local)
+# git commit --amend
---
+++ script_docs/missbehave.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# brew install htop
+# brew install cpulimit
+# brew install pidof
+
+### Check system status
+# htop
+
+### Search process
+# ps | grep <name>
+
+### get PID
+#pidof <name>
+
+### restrict process with renice from -20 to 19
+### (the higher the nicer/less prioritized)
+# sudo renice <nice-val-integer> -p <PID>
+
+### restrict process with cpulimit
+### percent is between 0-400
+# cpulimit -l <procent> -p <PID>
+
+### Kill processes: -> Sequence start from top
+# kill -15 <PID> # sends sigterm
+# kill     <PID> # also sends sigterm
+# kill -2 <PID> # sends sigint
+# kill -1 <PID> # sends sighup
+# kill -9 <PID> # sends sigkill
