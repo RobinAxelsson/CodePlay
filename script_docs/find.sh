d4227940c7fca3f6c0c5f9327027bdf0d89a23a3
RobinAxelsson <axelsson.robin@outlook.com>
2021-09-14 19:08:06 +0200

    pbcopy

---
+++ script_docs/find.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# find ~/Code -name "*.js" | grep -v node | grep -iF roman
+### gets javascript files without node in path, filtered for case insensitive roman
---
+++ script_docs/pipetoclip.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# echo send-to-clip | pbcopy
+# ^v
