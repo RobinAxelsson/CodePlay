29120f335c131c029e99f89ec92bfc8c86fb6c97
RobinAxelsson <axelsson.robin@outlook.com>
2021-09-17 23:16:56 +0200

    dotnet runs

--- compile.sh
+++ compile.sh
@@ -2,3 +2,5 @@
 gcc hello.c -o helloc
 g++ hello.cpp -o hellocpp
 ilasm hello.il
+dotnet publish ./HelloCsharp/HelloCsharp.csproj
+dotnet publish ./HelloVB/HelloVB.vbproj
--- run-hello.sh
+++ run-hello.sh
@@ -10,6 +10,6 @@ node hello.js jsarg 2
 dotnet HelloCsharp/bin/Debug/net5.0/publish/HelloCsharp.dll csharpArg 2
 dotnet HelloVB/bin/Debug/net5.0/HelloVB.dll vbArg 2
 dotnet fsi hello.fsx fsxArg 2
-dotnet-script hello.csx -- csxArg 2
+dotnet script hello.csx -- csxArg 2
 java hello.java javarg 2
 pwsh hello.ps1 pwshArg 2
