e1c0a2fd4dc605f31a0e450a3fe6568fd2dde5fc
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-09-27 07:37:15 +0200

    cleanup added node

--- test-runner.sh
+++ test-runner.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
-export TestUrl="http://localhost:8080/"
+#export TestUrl="http://localhost:8080/"
 
 echo --------Making requests------------
-echo "---xunit-------->"
-echo "dotnet test"
-dotnet test
+# echo "---xunit-------->"
+# echo "dotnet test"
+# dotnet test
 echo "---csx---------->"
-echo "dotnet script request.csx"
 dotnet script request.csx
 echo "---powershell--->"
-echo "pwsh request.ps1"
 pwsh request.ps1
 echo "---python------->"
-echo "python3 request.py"
 python3 request.py
+echo "---js/nodejs---->"
+node request.js
 echo "---curl/bash---->"
-echo "bash request.sh"
 bash request.sh
 
-unset TestUrl
+#unset TestUrl
