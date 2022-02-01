f5fade84145c40dab5dc94238353047fa9a06e64
RobinAxelsson <axelsson.robin@outlook.com>
2022-02-02 00:01:46 +0100

    added hello world

--- HelloVB/Program.vb
+++
@@ -1,7 +0,0 @@
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("VB: Hello!" + args(0) + " " + args(1))
-    End Sub
-End Module
---
+++ HelloWorld/HelloCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"C#: Hello! {args[0]} {args[1]}");
+        }
+    }
+}
