c21958350d3b6cad5ab012236fe050ae452f6b7d
RobinAxelsson <axelsson.robin@outlook.com>
2022-02-02 15:25:04 +0100

    added ps1 to hello

---
+++ HelloWorld/run-hello.ps1
@@ -0,0 +1,16 @@
+Write-Host "--------------------------------------------"
+
+.\helloc cArg 2
+.\hellocpp cppArg 2
+# Smono hello.exe
+#.\hello.zsh zsharg 2
+python hello.py pyarg 2
+# npx ts-node hello.ts
+node hello.js jsarg 2
+dotnet HelloCsharp\bin\Debug\net5.0\publish\HelloCsharp.dll csharpArg 2
+dotnet HelloVB\bin\Debug\net5.0\HelloVB.dll vbArg 2
+dotnet fsi hello.fsx fsxArg 2
+dotnet script hello.csx -- csxArg 2
+java hello.java javarg 2
+pwsh hello.ps1 pwshArg 2
+dotnet msbuild hello.proj
