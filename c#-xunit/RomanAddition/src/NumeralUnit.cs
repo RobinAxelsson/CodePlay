6d149d2073e9ff94d71459bcda03eb0c480cf041
Robin Axelsson <RAXELSS6@volvocars.com>
2022-11-20 18:29:51 +0100

    added numeral

---
+++ c#-xunit/RomanAddition/src/NumeralUnit.cs
@@ -0,0 +1,13 @@
+﻿namespace RomanAddition;
+
+public abstract class NumeralUnit
+{
+    public string ShortValue { get; }
+    public string LongValue { get; }
+    protected NumeralUnit(string shortValue, string longValue)
+    {
+        ShortValue = shortValue;
+        LongValue = longValue;
+    }
+    protected NumeralUnit Left { get; }
+}
